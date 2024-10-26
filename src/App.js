b88e999a07cd4fd79afd41ec69b77b02463ad500
QbicSquid <shifan611@gmail.com>
2024-10-26 13:40:11 +0530

    feat: domain and research gap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // * Components
 import Hero from "./components/Hero";
 import Reviews from "./components/Reviews";
-import Guide from './components/Guide'
 import Properties from "./components/Properties";
 import MoreDetail from "./components/MoreDetail";
 import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-
+import Domain from "./components/Domain";
+import ResearchGap from "./components/ResearchGap";
 
 function App() {
-
   return (
     <div className="App">
       <Hero />
+      <Domain />
+      <ResearchGap />
       <Reviews />
-      <Guide />
       <Properties />
       <MoreDetail />
       <Featured />
